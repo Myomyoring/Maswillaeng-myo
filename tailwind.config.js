a37dcc4de0c6800add217c3561d309dca8d4eb76
Myomyoring <narcissuscat@naver.com>
2023-07-19 17:54:26 +0900

    feat : CRA에서 Vite으로 마이그레이션 및 라이브러리 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  colors: {
-    main: "#FBF9EC",
-    point: "#EA4E4E",
-    red: {
-      500: "#EA4E4E",
-    },
-  },
   plugins: [],
-};
+}
