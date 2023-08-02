74eda3c65520f400ce0ead5ce2cc7131df89e9fb
Myomyoring <narcissuscat@naver.com>
2023-08-02 19:50:50 +0900

    feat : 회원가입 페이지 기능, 정규표현식 룰, 이미지 미리보기 기능

--- src/components/signUp/ImageInput.jsx
+++ src/components/signUp/ImageInput.jsx
@@ -1,67 +1,60 @@
 import React, { useRef, useState } from 'react';
 import { styled } from 'styled-components';
 import tw from 'twin.macro';
-import DefaultUserImage from '../../statics/images/default_user_image.jpg'
-import AddFileIcon from '../../statics/svg/addFileIcon'
+import AddFileIcon from '../../statics/svg/addFileIcon';
 
 const InputBox = styled.div`
-    ${tw`
+  ${tw`
+        relative
         mx-auto
         text-center
     `}
-`
+`;
 
 const Image = styled.img`
-    ${tw`
+  ${tw`
         w-40 h-40
         rounded-full
         border-solid border-gray border-[1px]
     `}
-
-`
+`;
 const Input = styled.input`
-    ${tw`
+  ${tw`
          hidden
     `}
-`
+`;
 const Label = styled.label`
-    ${tw`
+  ${tw`
         cursor-pointer
     `}
-    svg {
-        ${tw`
-            // absolute
-            // top-[19rem] right-[38.5rem]
+  svg {
+    ${tw`
+            absolute
+            top-32 right-28
         `}
-    }
-`
+  }
+`;
 
 export default function ImageInput({ image }) {
-    const [imgFile, setImgFile] = useState('');
-    const imgRef = useRef();
+  const [imgFile, setImgFile] = useState('');
+  const imgRef = useRef();
 
-    const saveImgFile = () => {
-        const file = imgRef.current.files[0];
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onloadend = () => {
-        setImgFile(reader.result);
-   	};
-    }
+  const saveImgFile = () => {
+    const file = imgRef.current.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImgFile(reader.result);
+    };
+  };
 
-    return (
-        <InputBox>
-            <Image src={ imgFile ? imgFile : DefaultUserImage } alt="프로필 이미지" />
-            <Input
-                type="file"
-                id="preview"
-                accept="image/*"
-                onChange={ saveImgFile }
-                ref={ imgRef }
-                />
-            <Label htmlFor="preview">
-                <AddFileIcon />
-            </Label>
-        </InputBox>
-    );
+  return (
+    <InputBox>
+      <Image src={imgFile ? imgFile : image} alt="프로필 이미지" />
+      <Input type="file" id="preview" accept="image/*" onChange={saveImgFile} ref={imgRef} />
+      <Label htmlFor="preview">
+        <AddFileIcon />
+      </Label>
+    </InputBox>
+  );
 }
