09f8ce5fe7c7e40acaa6c6f7b8c6bf9d4356fecd
Myomyoring <narcissuscat@naver.com>
2023-12-21 20:56:25 +0900

    feat: 좋아요 등록, 유저 좋아요

--- src/services/firebaseService/like.firebase.service.jsx
+++ src/services/firebaseService/like.firebase.service.jsx
@@ -13,14 +13,20 @@ import {
 import { db } from '../../firebase-config';
 
 export const likeService = {
+  createLikeDoc({ postId }) {
+    return setDoc(doc(db, 'likes', postId), { postId, likeUsers: [] });
+  },
   getLikes({ postId }) {
     return getDocs(query(collection(db, 'likes'), where('postId', '==', postId)));
   },
+  getUserLikes({ userId }) {
+    return getDocs(query(collection(db, 'likes'), where('likeUsers', 'array-contains', userId)));
+  },
   saveLike({ postId, userId }) {
-    return setDoc(doc(db, 'likes', postId), { postId, likeUsers: arrayUnion(userId) });
+    return updateDoc(doc(db, 'likes', postId), { postId, likeUsers: arrayUnion(userId) });
   },
   deleteLike({ postId, userId }) {
-    return setDoc(doc(db, 'likes', postId), { postId, likeUsers: arrayRemove(userId) });
+    return updateDoc(doc(db, 'likes', postId), { postId, likeUsers: arrayRemove(userId) });
   },
   addLike({ postId, likeCnt }) {
     return updateDoc(doc(db, 'posts', postId), { likeCnt: likeCnt + 1 });
