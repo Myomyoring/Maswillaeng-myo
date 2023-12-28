27a72807b9b86a7063d044c54dbe2405b0827be3
Myomyoring <narcissuscat@naver.com>
2023-12-28 20:48:44 +0900

    refactor: 로고 컴포넌트 리팩토링

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 
-const LogoStyle = styled(Link)`
+const LogoStyle = styled.div`
+  ${tw``}
+`;
+const Text = styled.span`
   ${tw`
-      font-bold
-  `}
-  span {
-    ${tw`
-        text-3xl
+        font-bold text-3xl text-white
     `}
-  }
 `;
 
 export default function Logo() {
   return (
-    <LogoStyle to={'/'}>
-      <span>Mashillaeng</span>
+    <LogoStyle>
+      <Link to={'/'}>
+        <Text>Mashillaeng</Text>
+      </Link>
     </LogoStyle>
   );
 }
