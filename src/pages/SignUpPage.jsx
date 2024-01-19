6b81cc3927dc70daaa52bdda868370f03de22a86
Myomyoring <narcissuscat@naver.com>
2024-01-19 18:50:15 +0900

    refactor: 회원가입 페이지 스타일 분리

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,22 +1,13 @@
 import Logo from '../components/common/Logo';
 import SignUpForm from '../components/signUp/containers/SignUpForm.container';
 
-import { styled } from 'styled-components';
-import tw from 'twin.macro';
-
-const SignUpPageStyle = styled.div`
-  ${tw`
-      w-[500px]
-      mx-auto p-10
-      text-center
-    `}
-`;
+import * as S from './styles/index';
 
 export default function SignUpPage() {
   return (
-    <SignUpPageStyle>
+    <S.SignUpPageStyle>
       <Logo size="big" />
       <SignUpForm />
-    </SignUpPageStyle>
+    </S.SignUpPageStyle>
   );
 }
