c16c2a73ae94da9ad7bfc112004ef2202bfe1973
Myomyoring <narcissuscat@naver.com>
2024-01-26 19:59:19 +0900

    refactor: 게시판 헤더 스타일 분리

--- src/components/board/BoardHeader.jsx
+++ src/components/board/BoardHeader.jsx
@@ -1,37 +1,14 @@
 import BoardWriteButton from '../common/LinkButton';
 
-import { styled } from 'styled-components';
-import tw from 'twin.macro';
+import * as S from './style/BoardHeader.style';
 import WriteIcon from '../../statics/svg/write_icon';
-
-const BoardHeaderStyle = styled.header`
-  ${tw`
-        w-full h-16
-        flex justify-end items-center
-    `}
-
-  span {
-    ${tw`
-        flex items-center
-      text-white
-    `}
-  }
-
-  svg {
-    ${tw`
-      fill-white
-    `}
-  }
-`;
 
 export default function BoardHeader() {
   return (
-    <BoardHeaderStyle>
+    <S.BoardHeaderStyle>
       <BoardWriteButton to={`/boardWrite`}>
-        <span>
-          <WriteIcon /> 글쓰기
-        </span>
+        <WriteIcon /> 글쓰기
       </BoardWriteButton>
-    </BoardHeaderStyle>
+    </S.BoardHeaderStyle>
   );
 }
