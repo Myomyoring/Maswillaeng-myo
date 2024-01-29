afffeced2671bbca56fa4820b57b9115e65da933
Myomyoring <narcissuscat@naver.com>
2024-01-29 20:40:40 +0900

    style: 상세페이지 스타일 분리

--- src/pages/BoardDetailPage.jsx
+++ src/pages/BoardDetailPage.jsx
@@ -1,22 +1,11 @@
 import PostContents from '../components/boardDetail/containers/PostContents.container';
 
-import { styled } from 'styled-components';
-import tw from 'twin.macro';
-
-const BoardDetailPageStyle = styled.div`
-  ${tw`
-        h-full
-        mx-auto py-20
-
-        desktop:w-2/3
-        mobile:px-3
-    `}
-`;
+import * as S from './styles/BoardDetail.style';
 
 export default function BoardDetailPage() {
   return (
-    <BoardDetailPageStyle>
+    <S.BoardDetailPageStyle>
       <PostContents />
-    </BoardDetailPageStyle>
+    </S.BoardDetailPageStyle>
   );
 }
