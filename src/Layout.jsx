07bbd0ced05dca8c73f2ca999dbc3620e69cc409
Myomyoring <narcissuscat@naver.com>
2024-02-16 23:03:26 +0900

    fix: 자동 로그아웃 함수 적용

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
-
 import { authService } from './firebase-config';
 
-import Header from './components/common/Header';
+import { useAuth } from './contexts/ProvideAuthContext';
+import Header from './components/header/Header';
 import LoadingScreen from './components/common/LoadingScreen';
 
 export default function App() {
   const [isLoading, setLoading] = useState(true);
+  const { logoutTimer } = useAuth();
+
   const init = async () => {
     await authService.authStateReady();
     setLoading(false);
   };
+
   useEffect(() => {
     init();
+    logoutTimer();
   }, []);
   return (
     <>
